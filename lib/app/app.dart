2b923d1b1fc48696f5f4847ae962b4f65e72d204
IbrohimToxtasinov <toxtasinovibrohim976@gmail.com>
2023-03-14 11:24:51 +0500

    add

---
+++ lib/app/app.dart
@@ -0,0 +1,63 @@
+import 'package:easy_localization/easy_localization.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
+import '../../utils/constants.dart';
+import 'package:adaptive_theme/adaptive_theme.dart';
+import '../ui/router.dart';
+import '../utils/theme.dart';
+
+class App extends StatelessWidget {
+  const App({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+
+    return
+    // MultiBlocProvider(
+    //   providers: [
+    //     BlocProvider(create: (context) => GetUserCubit()),
+    //     BlocProvider(create: (context) => GetHomeDataCubit()),
+    //     BlocProvider(create: (context) => BottomNavCubit()),
+    //     BlocProvider(create: (context) => ConnectivityCubit()),
+    //     BlocProvider(create: (context) => LoginBloc()),
+    //     BlocProvider(create: (context) => SignUpBloc()),
+    //     BlocProvider(create: (context) => getIt<NotificationCacherBloc>()),
+    //     BlocProvider(create: (context) => getIt<NotificationReaderBloc>()),
+    //     BlocProvider(create: (context) => getIt<CategoryCubit>()),
+    //
+    //   ],
+    //   child:
+    MyApp()
+    // )
+    ;
+  }
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return ScreenUtilInit(
+      designSize: const Size(375, 812),
+      minTextAdapt: true,
+      splitScreenMode: true,
+      builder: (context, child) => AdaptiveTheme(
+        light: AppTheme.lightTheme,
+        dark: AppTheme.darkTheme,
+        initial: AdaptiveThemeMode.light,
+        builder: (light, dark) => MaterialApp(
+          localizationsDelegates: context.localizationDelegates,
+          supportedLocales: context.supportedLocales,
+          locale: context.locale,
+          title: 'Zamin Education',
+          theme: light,
+          darkTheme: dark,
+          debugShowCheckedModeBanner: false,
+          onGenerateRoute: MyRouter.generateRoute,
+          initialRoute: splashPage,
+        ),
+      ),
+    );
+  }
+}
--- lib/main.dart
+++ lib/main.dart
@@ -1,20 +1,43 @@
+import 'package:easy_localization/easy_localization.dart';
+import 'package:firebase_core/firebase_core.dart';
+import 'package:firebase_messaging/firebase_messaging.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:open_work/app/app.dart';
+import 'package:open_work/app/bloc_observer.dart';
 
-void main() {
-  runApp(const MyApp());
-}
-
-class MyApp extends StatelessWidget {
-  const MyApp({super.key});
-
-  @override
-  Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Flutter Demo',
-      theme: ThemeData(
-        primarySwatch: Colors.blue,
-      ),
-      // home:
-    );
-  }
+Future<void> main() async {
+  SystemChrome.setSystemUIOverlayStyle(
+    const SystemUiOverlayStyle(
+      statusBarIconBrightness: Brightness.dark,
+      statusBarColor: Colors.transparent, // status bar color
+    ),
+  );
+  // setup();
+  // WidgetsFlutterBinding.ensureInitialized();
+  // await Firebase.initializeApp();
+  // await FirebaseMessaging.instance.subscribeToTopic("zamin_news");
+  // FirebaseMessaging.onBackgroundMessage(getIt<NotificationService>().firebaseMessagingBackgroundHandler);
+  Bloc.observer = AppBlocObserver();
+  // await StorageRepository.getInstance();
+  await EasyLocalization.ensureInitialized();
+  SystemChrome.setPreferredOrientations([
+    DeviceOrientation.portraitUp,
+    DeviceOrientation.portraitDown,
+  ]);
+  runApp(
+    EasyLocalization(
+      supportedLocales: const [
+        Locale('en', 'EN'),
+        Locale('ru', 'RU'),
+        Locale('uz', 'UZ'),
+      ],
+      //fallbackLocale: const Locale('uz', 'UZ'),
+      startLocale:  const Locale('en','EN'),
+      path: "assets/translations",
+      //saveLocale: true,
+      child: App(),
+    ),
+  );
 }
