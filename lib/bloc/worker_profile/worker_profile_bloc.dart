7b7dc2d62c15f4efa6ba2e0cbd32d16c112b3889
Asus <samandarahadjonov@gmail.com>
2023-03-16 13:11:25 +0500

    get workerInfo method done inside of the bloc

--- lib/bloc/worker_profile/worker_profile_bloc.dart
+++ lib/bloc/worker_profile/worker_profile_bloc.dart
@@ -1,19 +1,23 @@
-import 'dart:async';
-
-import 'package:bloc/bloc.dart';
-import 'package:equatable/equatable.dart';
+import 'package:open_work/bloc/busynesses/busynesses_bloc.dart';
+import 'package:open_work/utils/file_importer/file_importer.dart';
 
 part 'worker_profile_event.dart';
 part 'worker_profile_state.dart';
 
 class WorkerProfileBloc extends Bloc<WorkerProfileEvent, WorkerProfileState> {
-  WorkerProfileBloc() : super(WorkerProfileState(status: WorkerStatus.PURE)) {
+  WorkerProfileBloc() : super(WorkerProfileState(status: WorkerStatus.PURE,worker: null,errorMessage: '')) {
     on<GetWorkerInfoEvent>(getWorkerInfo);
   }
 
 
-  void getWorkerInfo(event, emit) {
-    // emit()
+  Future<void> getWorkerInfo(event, emit) async {
+    emit(state.copyWith(status: WorkerStatus.GETTINGWORKERINFOINPROGRESS));
+    MyResponse myResponse = await getIt<WorkerRepository>().getWorkerInfo();
+    if(myResponse.errorMessage.isEmpty){
+      emit(state.copyWith(status: WorkerStatus.GETTINGWORKERINFOINSUCCESS,worker: myResponse.data));
+    }else{
+      emit(state.copyWith(status: WorkerStatus.GETTINGWORKERINFOINFAILURY,errorMessage: myResponse.errorMessage));
+    }
 
   }
 }
