8e7f8a882ff9ed444c4e6270ca3b1290b27a91d7
Abdusamad <abdukarimovabdusamad1998@gmail.com>
2023-03-23 16:52:54 +0500

    some changes

--- lib/ui/splash/splash_screen.dart
+++ lib/ui/splash/splash_screen.dart
@@ -19,7 +19,7 @@ class SplashScreen extends StatelessWidget {
         if (state.authStatus == AuthStatus.authenticated) {
           if (state.userRole == UserRole.client) {
             Navigator.pushNamedAndRemoveUntil(
-                context, workerDetail, (route) => false);
+                context, clientTabBox, (route) => false);
           } else {
             Navigator.pushNamedAndRemoveUntil(
                 context, workerTabBox, (route) => false);
