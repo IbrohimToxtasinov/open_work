d68183105ce9209795c65eab4ee39971e856fe06
rasulov999 <rasulovabdurashid89@gmail.com>
2023-03-28 12:24:24 +0500

    dd

--- lib/data/repositories/confirmation_repo.dart
+++ lib/data/repositories/confirmation_repo.dart
@@ -1,5 +1,4 @@
 import 'package:open_work/data/models/my_response/my_response_model.dart';
-import 'package:open_work/data/models/update_user_dto/update_user_dto_model.dart';
 import 'package:open_work/services/get_it.dart';
 import 'package:open_work/services/network/api_service/api_service.dart';
 
